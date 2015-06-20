199ff9add63ea6c52f58e7475da7f5cda304a6c8
hacka-slash <placido.hoff@gmail.com>
2015-06-20 13:10:34 -0500

    team-detail.ctrl edit with lodash

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # eliteApp
-Fixed links from teams page to team-detail. Did This using an 'anchor' tag over my div.
-The anchor has the ng-repeat directive, then with it, I have access to the 
-team.id variable, which gets passed to the team-detail.ctrl thru the $stateParams.
+Fixing TeamDetails page by fixing its controller and making use of 'lodash'
+Had to edit the original lodash code slightly but got the display working
--- www/templates/teams/team-detail-ctrl.js
+++ www/templates/teams/team-detail-ctrl.js
@@ -1,13 +1,112 @@
 
-(function() { 
+(function() {
 	'use strict';
 	
-	angular.module('starter.controllers').controller('teamDetailCtrl', ['$stateParams', teamDetailCtrl]);
+	angular.module('starter.controllers').controller('teamDetailCtrl', ['$stateParams','eliteApi', teamDetailCtrl]);
 	
-	function teamDetailCtrl($stateParams) {
+	function teamDetailCtrl($stateParams, eliteApi) {
 		var vm = this;
+        
 		console.log("$stateParams", $stateParams);
-	
+        
+        vm.teamId = Number($stateParams.id);
+        
+        var data = eliteApi.getLeagueData();
+        
+        //var team = _.chain(data.teams).filter({"divisionName":"5th Grade - Green"}).value();
+        
+        var team = _.chain(data.teams)
+                    .pluck("divisionTeams")
+                    .flatten().filter({"id":vm.teamId})
+                    .value();
+        
+        vm.teamName = team[0].name;
+        
+        vm.games = _.chain(data.games)
+                    .filter(isTeamInGame)
+                    .map(function(item) {
+                            
+                            var isTeam1 = (item.team1Id === vm.teamId ? true : false);
+                            
+                            var opponentName = (isTeam1 ? item.team2 : item.team1);
+            
+                            var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score, item.team2Score);
+                                                               
+                            return {
+                                gameId: item.id,
+                                opponent: opponentName,
+                                time: item.time,
+                                location: item.location,
+                                locationUrl: item.locationUrl,
+                                scoreDisplay: scoreDisplay,
+                                homeAway: (isTeam1 ? 'vs' : 'at')
+                            };
+            
+                        }
+                        )
+                    .value();
+        
+        console.log(vm.games[0]);
+        
+        function isTeamInGame(item) {
+            return item.team1Id === vm.teamId || item.item2Id === vm.teamId;   
+        };
+        
+        function getScoreDisplay(isTeam1, team1Score, team2Score) {
+            if(team1Score && team2Score){
+                var teamScore = (isTeam1 ? team1Score : team2Score);
+                var opponentScore = (isTeam1 ? team2Score : team1Score);
+                var winIndicator = teamScore > opponentScore ? "W" : "L";
+                return winIndicator + teamScore + "-" + opponentScore;
+            }else{
+             return "";   
+            }
+        };
+        
+        /*
+        
+        vm.games = _.chain(data.games)
+                        .filter(isTeamInGame)
+                        .map(function(item) {
+                            
+                            var isTeam1 = (item.team1 === vm.teamId ? true : false);
+                            
+                            var opponentName = (isTeam1 ? item.team2 : item.team1);
+            
+                            var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score, item.team2score);
+                                                               
+                            return {
+                                gameId: item.id,
+                                opponent: opponentName,
+                                time: item.time,
+                                location: item.location,
+                                locationUrl: item.locationUrl,
+                                scoreDisplay: scoreDisplay,
+                                homeAway: (isTeam1 ? 'vs' : 'at')
+                            }
+            
+                        }
+                        )
+                        .value();
+        
+        //Supporting Functions:
+        
+        function isTeamInGame(item) {
+            return item.team1Id === vm.teamId || item.item2Id === vm.teamId;   
+        };
+        
+        function getScoreDisplay(isTeam1, team1Score, team2Score) {
+            if(team1Score && team2Score){
+                var teamScore = (isTeam1 ? team1Score : team2Score);
+                var opponentScore = (isTeam1 ? team2Score : team1Score);
+                var winIndicator = teamScore > opponentScore ? "W" : "L";
+                return winIndicator + teamScore + "-" + opponentScore;
+            }else{
+             return "";   
+            }
+        };*/
+        
+	   
 	}
 
 }) ();
